c4b807409089999030983719f3d1de1734e315c0
Max <max@krnl.xyz>
2025-02-05 11:16:30 +0700

    Added .env example

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 # Twitter API v2 credentials
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
-V2_CLIENT_ID = os.getenv("TWITTER_CLIENT_ID")
-V2_CLIENT_SECRET = os.getenv("TWITTER_CLIENT_SECRET")
-V2_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
-V2_ACCESS_SECRET = os.getenv("TWITTER_ACCESS_SECRET")
+V2_CLIENT_ID = os.getenv("V2_CLIENT_ID")
+V2_CLIENT_SECRET = os.getenv("V2_CLIENT_SECRET")
+V2_ACCESS_TOKEN = os.getenv("V2_ACCESS_TOKEN")
+V2_ACCESS_SECRET = os.getenv("V2_ACCESS_SECRET")
 
 # Scheduling intervals (in seconds)
 POST_INTERVAL_MIN = 1800   # 30 min
