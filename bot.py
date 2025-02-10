b505871ae4d998629122dd21a87e86e2d99fdc5c
Max <max@krnl.xyz>
2025-02-10 11:38:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,31 +31,28 @@ logging.basicConfig(filename="logs/bot.log", level=logging.INFO)
 # ----------------------------------------------------
 # Twitter API v2 credentials
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
-V2_CLIENT_ID = os.getenv("V2_CLIENT_ID")
-V2_CLIENT_SECRET = os.getenv("V2_CLIENT_SECRET")
-V2_ACCESS_TOKEN = os.getenv("V2_ACCESS_TOKEN")
-V2_ACCESS_SECRET = os.getenv("V2_ACCESS_TOKEN_SECRET")
+CONSUMER_KEY = os.getenv("V2_CLIENT_ID")
+CONSUMER_SECRET = os.getenv("V2_CLIENT_SECRET")
+ACCESS_TOKEN = os.getenv("V2_ACCESS_TOKEN")
+ACCESS_SECRET = os.getenv("V2_ACCESS_TOKEN_SECRET")
 
 API_KEY = os.getenv("API_KEY")
 API_KEY_SECRET = os.getenv("API_KEY_SECRET")
 
-print("CONSUMER_KEY:", repr(V2_CLIENT_ID))
-print("CONSUMER_SECRET:", repr(V2_CLIENT_SECRET))
-print("ACCESS_TOKEN:", repr(V2_ACCESS_TOKEN))
-print("ACCESS_SECRET:", repr(V2_ACCESS_SECRET))
+# Print out the credentials (or parts of them) for debugging purposes.
+print("CONSUMER_KEY:", repr(CONSUMER_KEY))
+print("CONSUMER_SECRET:", repr(CONSUMER_SECRET))
+print("ACCESS_TOKEN:", repr(ACCESS_TOKEN))
+print("ACCESS_SECRET:", repr(ACCESS_SECRET))
 print("BEARER_TOKEN:", repr(BEARER_TOKEN))
 
-print("API_KEY:", repr(API_KEY))
-print("API_KEY_SECRET:", repr(API_KEY_SECRET))
-
+# Create a Tweepy client using the provided credentials
 client = tweepy.Client(
     bearer_token=BEARER_TOKEN,
-    # consumer_key=V2_CLIENT_ID,
-    # consumer_secret=V2_CLIENT_SECRET,
     consumer_key=API_KEY,
     consumer_secret=API_KEY_SECRET,
-    access_token=V2_ACCESS_TOKEN,
-    access_token_secret=V2_ACCESS_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_SECRET,
     wait_on_rate_limit=True
 )
 
